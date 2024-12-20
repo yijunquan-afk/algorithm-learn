eb37eb6a29f50b2863ebeed74556aa557613f05b
junquanyi <junquanyi@tencent.com>
2024-12-20 22:34:30 +0800

    2024-12-20

---
+++ src/array/longestOnes3/Solution.java
@@ -0,0 +1,17 @@
+package array.longestOnes3;
+
+/**
+ * ClassName:Solution
+ * Package:array.longestOnes3
+ * Description:
+ *
+ * @date:2024/12/20 22:25
+ * @author: Junquan Yi
+ */
+public class Solution {
+    public int longestOnes(int[] nums, int k) {
+        int longestNum = 0;
+
+        return longestNum;
+    }
+}
